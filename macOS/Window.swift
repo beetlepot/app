3596938369f7904772de3b598992338a3d233eb2
vaux <zero.griffin@gmail.com>
2021-11-26 17:24:43 +0100

    Bump version

--- macOS/Window.swift
+++ macOS/Window.swift
@@ -76,50 +76,6 @@ final class Window: NSWindow, NSWindowDelegate {
                 view.bottomAnchor.constraint(equalTo: content.bottomAnchor).isActive = true
             }
             .store(in: &subs)
-        
-        Task {
-//            await cloud.add(purchase: .ten)
-//            try! await cloud.secret()
-//            try! await cloud.secret()
-//            try! await cloud.secret()
-//            try! await cloud.secret()
-//            try! await cloud.secret()
-//            await cloud.update(id: 0, name: "Gran's secret Shortbread Recipe")
-//            await cloud.update(id: 0, payload: """
-//            **Ingredients**
-//            — 200g _butter_
-//            — 100g _caster sugar_
-//            — 300g _plain flour_
-//            — 1 level teaspoon _baking powder_
-//            — 1 level teaspoon _ginger_
-//            — 1 pinch _salt_
-//
-//            **Process**
-//            _1._ Cream butter and sugar.
-//            _2._ Sift in other ingredients and mix well.
-//            _3._ Spread in lined bake tray.
-//            _4._ Bake in moderate over circa 40 minutes until golden.
-//            _5._ Eat warm.
-//
-//            """)
-//            await cloud.add(id: 0, tag: .biscuits)
-//            await cloud.add(id: 0, tag: .cook)
-//            await cloud.add(id: 0, tag: .family)
-//            await cloud.add(id: 0, tag: .food)
-//
-//            await cloud.add(id: 1, tag: .hidden)
-//            await cloud.add(id: 1, tag: .keys)
-//
-//            await cloud.add(id: 2, tag: .important)
-//
-//            await cloud.update(id: 0, favourite: true)
-//            await cloud.update(id: 2, favourite: true)
-//
-//            await cloud.update(id: 1, name: "Office's door keycode")
-//            await cloud.update(id: 2, name: "The meaning of life")
-//            await cloud.update(id: 3, name: "Cezz's phone number")
-//            await cloud.update(id: 4, name: "The secret cookie stash")
-        }
     }
     
     override func close() {
--- watchOS Extension/Sidebar.swift
+++ watchOS Extension/Sidebar.swift
@@ -10,46 +10,6 @@ struct Sidebar: View {
         NavigationView {
             Items(filtered: filtered)
                 .searchable(text: $filter.search)
-//                .task {
-//                    await cloud.add(purchase: .ten)
-//                    try! await cloud.secret()
-//                    try! await cloud.secret()
-//                    try! await cloud.secret()
-//                    try! await cloud.secret()
-//                    try! await cloud.secret()
-//                    await cloud.update(id: 0, name: "Gran's secret Shortbread Recipe")
-//                    await cloud.update(id: 0, payload: """
-//**Ingredients**
-//— 200g _butter_
-//— 100g _caster sugar_
-//— 300g _plain flour_
-//— 1 level teaspoon _baking powder_
-//— 1 level teaspoon _ginger_
-//— 1 pinch _salt_
-//
-//**Process**
-//_1._ Cream butter and sugar.
-//_2._ Sift in other ingredients and mix well.
-//_3._ Spread in lined bake tray.
-//_4._ Bake in moderate over circa 40 minutes until golden.
-//_5._ Eat warm.
-//
-//""")
-//                    await cloud.add(id: 0, tag: .biscuits)
-//                    await cloud.add(id: 0, tag: .cook)
-//                    await cloud.add(id: 0, tag: .family)
-//                    await cloud.add(id: 0, tag: .food)
-//                    await cloud.add(id: 0, tag: .important)
-//                    await cloud.add(id: 0, tag: .top)
-//                    
-//                    await cloud.update(id: 0, favourite: true)
-//                    await cloud.update(id: 2, favourite: true)
-//                    
-//                    await cloud.update(id: 1, name: "Office's door keycode")
-//                    await cloud.update(id: 2, name: "The meaning of life")
-//                    await cloud.update(id: 3, name: "Cezz's phone number")
-//                    await cloud.update(id: 4, name: "The secret cookie stash")
-//                }
         }
         .onReceive(cloud) {
             filtered = $0.filtering(with: filter)
