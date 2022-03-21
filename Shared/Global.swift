1f5b133c517fa015da8b52a4f4a128323f27014d
vaux <zero.griffin@gmail.com>
2022-03-21 11:13:37 +0100

    Update packages

--- Shared/Global.swift
+++ Shared/Global.swift
@@ -1,5 +1,6 @@
 import Archivable
+import CloudKit
 import Secrets
 
-let cloud = Cloud<Archive>.new(identifier: "iCloud.beet")
+let cloud = Cloud<Archive, CKContainer>.new(identifier: "iCloud.beet")
 let store = Store()
