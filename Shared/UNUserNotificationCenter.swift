54cc30dd7a691801fd5f21254aefe2423ce6629a
vaux <zero.griffin@gmail.com>
2022-03-21 13:42:48 +0100

    Update notifications

--- Shared/UNUserNotificationCenter.swift
+++ Shared/UNUserNotificationCenter.swift
@@ -9,23 +9,26 @@ extension UNUserNotificationCenter {
     
     static func send(message: String) async {
         let settings = await current().notificationSettings()
-        guard settings.alertSetting == .enabled && settings.authorizationStatus == .authorized else { return }
+        guard settings.authorizationStatus == .authorized else { return }
         let content = UNMutableNotificationContent()
         content.body = message
         try? await current().add(.init(identifier: UUID().uuidString, content: content, trigger: nil))
     }
     
     static func request() async {
-        _ = try? await current().requestAuthorization(options: [.alert])
+        _ = try? await current().requestAuthorization(options: [.alert, .badge, .criticalAlert, .provisional])
     }
     
     func present(_ notification: UNNotification) async -> UNNotificationPresentationOptions {
         let delivered = await deliveredNotifications()
         removeDeliveredNotifications(withIdentifiers: delivered
                                         .map(\.request.identifier)
-                                        .filter {
-                                            $0 != notification.request.identifier
-                                        })
-        return .banner
+                                        .filter { $0 != notification.request.identifier })
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) { [weak self] in
+            self?.removeAllDeliveredNotifications()
+        }
+        
+        return notification.request.content.userInfo["aps"] == nil ? .banner : []
     }
 }
